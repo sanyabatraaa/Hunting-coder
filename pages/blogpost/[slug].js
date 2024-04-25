608ea1aa2682cbc87de66e511b98406451618172
sanyabatraaa <151373533+sanyabatraaa@users.noreply.github.com>
2024-04-25 22:20:13 +0530

    Update [slug].js

--- pages/blogpost/[slug].js
+++ pages/blogpost/[slug].js
@@ -1,8 +1,7 @@
 import React ,{useState,useEffect} from 'react'
 import {useRouter} from 'next/router'; 
 import styles from '../../styles/BlogPost.module.css';
-//1.find the file corresponding to the slug
-//2. populate them inside yhe page
+
 const slug = ({allBlogs}) => {
   function createMarkup(c) {
     return {__html: c};
@@ -10,17 +9,7 @@ const slug = ({allBlogs}) => {
  
   const [blog,setBlog]=useState(allBlogs);
   const router=useRouter();
-    // useEffect(()=>{
-    //   if(!router.isReady) return;
-    //   const {slug}=router.query;
-    //   fetch(`http://localhost:3000/api/getBlog?slug=${slug}`).then((a)=>{
-    //     return a.json();
-    //   })
-    //   .then((parsed)=>{
-    //     setBlog(parsed)
-    //   })
-    // },[router.isReady]);
-    // const {slug}=router.query;
+    
   return (
     <div className={styles.container}>
       <main className={`${styles.main}`}>
