520184aca28882fd00d520bba1380d50513c296c
sanyabatraaa <151373533+sanyabatraaa@users.noreply.github.com>
2024-04-25 22:19:33 +0530

    Update getBlog.js

--- pages/api/getBlog.js
+++ pages/api/getBlog.js
@@ -1,5 +1,4 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-//http://localhost:3000/api/getBlog?slug=how-to-learn-flask
+
 import * as fs from 'fs';
 export default function handler(req, res) {
   fs.readFile(`blogdata/${req.query.slug}.json`,'utf-8',(err,data)=>{
@@ -8,5 +7,5 @@ export default function handler(req, res) {
     }
     res.status(200).json(JSON.parse(data));
   })
-  // res.status(200).json({ name: "John Doe" });
+  
 }
